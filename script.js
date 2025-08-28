9861a3f36394adcc4343d901b656880a526e78c3
vidya <159389388+Vidyasravanthigarikapati@users.noreply.github.com>
2025-08-27 19:32:35 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
+// Initialize the map
 var map = L.map('map').setView([37.7, -122.4], 10);
 
-  // load a tile layer
+// Load a tile layer
 L.tileLayer('https://tiles.stadiamaps.com/tiles/stamen_toner/{z}/{x}/{y}{r}.png', {
   attribution: 'Map tiles by Stamen Design via Stadia Maps, under CC BY 3.0.',
   maxZoom: 20,
   detectRetina: true
 }).addTo(map);
-
- 
 
 // Define the custom rat icon
 var ratIcon = L.icon({
